d835dab44c982d5f7cc767359bfab7c109083e68
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-09 15:59:17 +0700

    Chore: move to new folder

--- config/connect_db.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('DB', 'learn_restful_api');
-    define('PASS', '');
-
-    $conn = new mysqli(HOST, USER, PASS, DB) or die('Could not connect to MySQL: ' . mysqli_connect_error());
-
-?>
